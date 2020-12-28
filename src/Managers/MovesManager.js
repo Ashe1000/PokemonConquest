b6b97314c8ccbf584ede6bab0a2236baa676a7a9
Illya9999 <ib66189276@gmail.com>
2020-12-28 14:29:06 -0500

    added findXY
    this was big math

--- src/Managers/MovesManager.js
+++ src/Managers/MovesManager.js
@@ -1,4 +1,25 @@
+/*
+30 19 1A 1B 1C 1D 1E
+2F 18 09 0A 0B 0C 1F
+2E 17 08 01 02 0D 20
+2D 16 07 00 03 0E 21
+2C 15 06 05 04 0F 22
+2B 14 13 12 11 10 23
+2A 29 28 27 26 25 24
 
+0 
+8
+16
+24
+
+48 25 26 27 28 29 30
+47 24 09 10 11 12 31
+46 23 08 01 02 13 32
+45 22 07 00 03 14 33
+44 21 06 05 04 15 34
+43 20 19 18 17 16 35
+42 41 40 39 38 37 36
+*/
 /*
 This is how knockback is calculated
 The middle, 00, would be like (0,0) on a grid. And as the value increases it spirals outwards
@@ -9,15 +30,56 @@ The middle, 00, would be like (0,0) on a grid. And as the value increases it spi
 2A 14 06 05 04 0E 20
 29 13 12 11 10 0F 21
 28 27 26 25 24 23 22
-
+17 18 09 0A 0B 1D
+2C 16 08 01 02 0C
+2B 15 07 00 03 0D
+2A 14 06 05 04 0E
+29 13 12 11 10 0F
 */
-//should go from knockback value to an array with [x, y]
+
+//value into [x,y]
 let findXY = val => {
 	
+	//get the Math.max(Math.abs(x), Math.abs(y))
+	let layer = Math.round((Math.sqrt(val) | 0) / 2);
+	//get the area of the layer under this one
+	let offset = (layer * 2 - 1) ** 2;
+	//get the amount of points on the layer
+	let permiter = layer * 8;
+	
+	/*
+		get the amount of positions on a face (faces are oriented like this)
+		0 0 3
+		1 - 3
+		1 2 2
+	*/
+	let faceSize = layer * 2;
+
+	/*
+	offset the input by the area of the prev layer
+	*/
+	let n = (val - offset + layer + 1) % permiter;
+
+	let faceThing = permiter - n;
+	// get what face the number is on (face number is changed by 1 so math works out better)
+	let face = ((faceThing / faceSize | 0) + 1) % 4;
+	//return [face, n, faceThing, faceSize, permiter]
+	let triangleWave = a =>
+		faceSize / Math.PI*Math.asin(Math.sin(Math.PI/faceSize*a));
+	if(face % 2) {
+		let mult = (face - 3) / 2 || 1;
+		return [mult * layer, mult * Math.round(triangleWave(n - permiter - layer - 1)) | 0]
+	}
+	let mult = -face / 2 || 1;
+	return [mult * Math.round(triangleWave(n - permiter - layer - 1)) | 0, mult * layer]
 }
+
 //goes from (x,y) -> knockback value
+//i also think its bugged? but i might have patched it
 let findVal = (x, y) => {
 	let layer = Math.max(Math.abs(x), Math.abs(y));
+	if(layer === 0)
+		return 0;
 	let posX = 0;
 	let posY = layer;
 	let value = ((layer - 1) * 2 + 1) ** 2;
@@ -40,9 +102,10 @@ let findVal = (x, y) => {
 	return value + size - 1 + x - posX;
 }
 
-//this is outdated
 let findValOG = (x, y) => {
 	let layer = Math.max(Math.abs(x), Math.abs(y));
+	if(layer === 0)
+		return 0;
 	let posX = 0;
 	let posY = layer;
 	let value = ((layer - 1) * 2 + 1) ** 2;
